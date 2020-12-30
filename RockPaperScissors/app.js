4eee4a63bb1e48c84f3c34c79e09b199a062e265
devlovegit <devin.hakola@gmail.com>
2020-12-30 12:20:41 -0800

    Git?

---
+++ RockPaperScissors/app.js
@@ -0,0 +1,23 @@
+// user selects from rock, paper, scissors
+// computer randomly selects a new move to battle user with
+// display image based on each players choices
+// keep track of scores
+// display a winner for each background
+// CACHED ELEMENT REFERENCES
+//-Images, Scores
+// STATE
+// -Each players scores
+// Winner
+// -Available images associated with each weapon
+// -The available weapons and which opponent they defeat
+// EVENT LISTENERS
+// -Clicking an attack
+// FUNCTIONS
+// -Respond to players selection
+// -Determine winner
+// -Display the results
+
+const pScore = document.getElementById("player-score");
+const cScore = document.getElementById("computer-score");
+const tScore = document.getElementById("tie-score");
+console.log(pScore, cScore, tScore);
---
+++ index.js
@@ -0,0 +1,10 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function myFunction() {
+  let color = "";
+  let arr = ['red', 'blue', 'green']
+
+  document.body.style.backgroundColor = "gray";
+}
