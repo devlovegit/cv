26d18a699330cf1834056343f775e50dae93905d
devlovegit <devin.hakola@gmail.com>
2021-01-26 15:19:45 -0800

    Nav-bar practice

---
+++ NavBars/horizontalnav/index.js
@@ -0,0 +1,34 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+        // Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade .5s ease forwards ${index / 7 + .8}s`
+            }
+        });
+        // Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+
+}
+
+navSlide();
+
+
+
+
+// const app = () => {
+//     navSlide();
+//     navSlide();
+//     navSlide();
+//     navSlide();
+// }
